e30641d882b229b85e9afc0e8957e921f3cb6a92
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2022-12-30 23:56:06 +0900

    221230_Update
    1일차

--- CondingTest_Solved/main.cpp
+++ CondingTest_Solved/main.cpp
@@ -1,5 +1,15 @@
 #define PROB 1
 
 #if PROB == 1
+// [1000] A+B
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	int input[2];
+	cin >> input[0] >> input[1];
+
+	cout << input[0] + input[1];
+}
 #endif
