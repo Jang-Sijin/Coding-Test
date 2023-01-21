5eba84d54800249d44e19d66e1cbc149f534dc25
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2023-01-21 23:58:42 +0900

    230121_Update
    15일차

--- CodingTest_Study/Level2.cpp
+++ CodingTest_Study/Level2.cpp
@@ -1,4 +1,4 @@
-#define PROB 1
+#define PROB 4
 
 #if PROB == 1
 // [중복 제거하기]
@@ -6,5 +6,65 @@ SELECT COUNT(DISTINCT NAME) AS count
 FROM ANIMAL_INS
 WHERE NAME IS NOT NULL
 
+#elif PROB == 2
+// [동물 수 구하기]
+SELECT count(*) as count
+FROM ANIMAL_INS
 
+#elif PROB == 3
+// [최솟값 구하기]
+SELECT DATETIME as "시간"
+FROM ANIMAL_INS
+ORDER BY DATETIME ASC
+LIMIT 1
+
+#elif PROB == 4
+// [최댓값과 최솟값]
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> GetSplit(string s)
+{
+    vector<string> result;
+    int i = 0;
+    while (s[i] > s.length())
+    {
+        if (s[i] == ' ')
+        {
+            string front = s.substr(0, i);
+            result.push_back(front);
+            s = s.substr(i + 1, s.length());
+        }
+
+        i++;
+    }
+
+    return result;
+}
+
+string solution(string s) 
+{
+    string answer = "";
+
+    vector<string> ss = GetSplit(s);
+
+    for (auto n : ss)
+    {
+        cout << n << endl;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution("1 2 3 4"); // result: "1 4"
+    solution("-1 -2 -3 -4"); // result: "-4 -1"
+    solution("-1 -1"); // result: "-1 -1"
+
+    return 0;
+}
 #endif
