c2e433b4f510b403e71b21a349971abd41a78653
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2023-01-23 22:55:03 +0900

    230123_Update
    16일차

--- CodingTest_Study/Level2.cpp
+++ CodingTest_Study/Level2.cpp
@@ -1,4 +1,4 @@
-#define PROB 4
+#define PROB 5
 
 #if PROB == 1
 // [중복 제거하기]
@@ -23,48 +23,79 @@ LIMIT 1
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-vector<string> GetSplit(string s)
+vector<int> GetSplit(string input, string delimiter)
 {
-    vector<string> result;
-    int i = 0;
-    while (s[i] > s.length())
-    {
-        if (s[i] == ' ')
-        {
-            string front = s.substr(0, i);
-            result.push_back(front);
-            s = s.substr(i + 1, s.length());
-        }
+	vector<int> result;
+	int index = 0;
+	while ((index = input.find(delimiter)) != string::npos)
+	{
+		string token = input.substr(0, index);
+		result.push_back(stoi(token));
+		input.erase(0, index + delimiter.length());
+	}
+	result.push_back(stoi(input));
 
-        i++;
-    }
-
-    return result;
+	return result;
 }
 
-string solution(string s) 
+string solution(string s)
 {
-    string answer = "";
+	string answer{ "" };
+	string delimiter{ " " };
 
-    vector<string> ss = GetSplit(s);
+	vector<int> result = GetSplit(s, delimiter);
+	sort(result.begin(), result.end());
 
-    for (auto n : ss)
-    {
-        cout << n << endl;
-    }
-
-    return answer;
+	answer = to_string(result.front()) + delimiter + to_string(result.back());	
+	return answer;
 }
 
 int main()
 {
-    solution("1 2 3 4"); // result: "1 4"
-    solution("-1 -2 -3 -4"); // result: "-4 -1"
-    solution("-1 -1"); // result: "-1 -1"
+	solution("1 2 3 4"); // result: "1 4"
+	solution("-1 -2 -3 -4"); // result: "-4 -1"
+	solution("-1 -1"); // result: "-1 -1"
 
-    return 0;
+	return 0;
 }
+
+#elif PROB == 5
+// [JadenCase 문자열 만들기]
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s) 
+{
+	string answer = "";
+
+	transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+	if (isalpha(s[0]))
+		s[0] = toupper(s[0]);
+
+	for (int i = 0; i < s.length(); ++i)
+	{
+		if ((s[i] == ' ') && isalpha(s[i + 1]))
+			s[i + 1] = toupper(s[i + 1]);
+	}	
+
+	return answer = s;
+}
+
+int main()
+{
+	solution("3people  unFollowed me"); // result: "3people Unfollowed Me"
+	solution("for the last week"); // result: "For The Last Week"
+
+	return 0;
+}
+
 #endif
