fd9f4d45bda3f2645966fcbff2a607699932d0dd
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2023-07-30 16:27:06 +0900

    230730_Update

--- CodingTest_Study/Level2.cpp
+++ CodingTest_Study/Level2.cpp
@@ -1,4 +1,4 @@
-#define PROB 8
+#define PROB 15
 
 #if PROB == 1
 // [중복 제거하기]
@@ -169,6 +169,306 @@ int main()
 	solution("(())()"); // true
 	solution(")()("); // false
 	solution("(()("); // false
+
+	return 0;
+}
+#elif PROB == 9
+// [이진 변환 반복하기]
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <bitset>
+
+#define MAX_BIT_SIZE 150000
+
+using namespace std;
+
+vector<int> solution(string s) 
+{
+	vector<int> answer;
+
+	int turn{ 0 };
+	int sum_earase_zero_count{ 0 };
+
+	while (true)
+	{
+		++turn;
+		
+		s.erase(remove_if(s.begin(), s.end(), [&sum_earase_zero_count](char ch)
+			{
+				if (ch == '0')
+				{
+					sum_earase_zero_count++;
+					return true;
+				}
+				else
+					return false;
+			}), s.end());
+
+		auto s_value{ s.length() };
+		int bit_size{ 0 };
+
+		while (s_value != 0)
+		{
+			++bit_size;
+			s_value >>= 1;
+		}
+
+		s = bitset<MAX_BIT_SIZE>(s.size()).to_string().substr(MAX_BIT_SIZE - bit_size);
+	
+		if (s == "1")
+			break;
+	}
+
+	answer.push_back(turn);
+	answer.push_back(sum_earase_zero_count);
+
+	return answer;
+}
+
+int main()
+{
+	solution("110010101001"); // [3,8]
+	solution("01110"); // [3,3]
+	solution("1111111"); // [4,1]
+
+	return 0;
+}
+#elif PROB == 10
+// [숫자의 표현]
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) 
+{
+	int answer{ 0 };
+	int loop_value{ 1 };
+
+	while (loop_value <= n)
+	{
+		int sum{ 0 };
+		for (int i = loop_value; i <= n; ++i)
+		{
+			sum += i;
+
+			if (sum == n)
+			{							
+				++answer;				
+				break;
+			}
+			else if (sum > n)
+			{				
+				break;
+			}
+		}
+		++loop_value;
+	}
+
+	return answer;
+}
+
+int main()
+{
+	solution(15); // result: 4
+
+	return 0;
+}
+#elif PROB == 11
+// [다음 큰 숫자]
+#include <iostream>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <algorithm>
+
+#define MAX_VALUE_SIZE 32
+
+using namespace std;
+
+int solution(int n) 
+{
+	int answer = n + 1;
+
+	auto n_string = bitset<MAX_VALUE_SIZE>(n).to_string();
+	auto count_n = count(n_string.begin(), n_string.end(), '1');
+
+	while (true)
+	{
+		auto answer_string = bitset<MAX_VALUE_SIZE>(answer).to_string();
+		auto count_answer = count(answer_string.begin(), answer_string.end(), '1');
+		//cout << "n_string: " << n_string << "answer_string: " << answer_string << endl;
+		//cout << "count_n: " << count_n << "count_answer: " << count_answer << endl;
+
+		if (count_n == count_answer)
+		{
+			break;
+		}
+		else
+			answer += 1;
+	}
+	cout << answer << endl;
+
+	return answer;
+}
+
+int main()
+{
+	solution(78); // result: 83
+	solution(15); // result: 23
+
+	return 0;
+}
+#elif PROB == 12
+// [피보나치 수]
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int n)
+{
+	int answer{ 0 };
+
+	int f_0{ 0 }; // 0
+	int f_1{ 1 }; // 1
+	answer += (f_0 + f_1);
+
+	for (int i = 2; i <= n; ++i)
+	{		
+		int f_2 = (f_0 + f_1) % 1234567;
+		answer = f_2;
+		f_0 = f_1;
+		f_1 = f_2;
+	}
+
+	return answer;
+}
+
+int main()
+{
+	cout << solution(3) << endl; // result: 2
+	cout << solution(5) << endl; // result: 5
+
+	return 0;
+}
+#elif PROB == 13
+#include <iostream>
+#include<string>
+using namespace std;
+
+int solution(string s)
+{
+	int answer = -1;
+
+	// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+	cout << "Hello Cpp" << endl;
+
+	return answer;
+}
+
+int main()
+{
+	cout << solution("baabaa") << endl; // result: 2
+	cout << solution("cdcd") << endl; // result: 5
+
+	return 0;
+}
+#elif PROB == 14
+// [타겟 넘버]
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+// 재귀적으로 최대 깊이까지 탐색하는 알고리즘, 가장 깊은 거리까지 탐색하는 알고리즘
+// is_sign == true -> '+', is_sign == false -> '-'
+void DFS(const vector<int>& numbers, const int& target, int index, int sum, bool is_sign, int& answer)
+{
+	// +, - 조건 판별 후 결과 값 연산
+	if (is_sign)
+		sum += numbers[index++];
+	else
+		sum -= numbers[index++];
+
+	// 탐색 완료? 미완료?
+	if (numbers.size() != index) // 더 탐색 가능 (모든 정점 방문 미완료)
+	{
+		DFS(numbers, target, index, sum, true, answer);
+		DFS(numbers, target, index, sum, false, answer);
+	}
+	else // 더 이상 탐색 불가능 (모든 정점 방문 완료)
+	{
+		if (sum == target) // 원하는 결과 값이 완성됨
+			answer++;
+		
+		return;
+	}
+}
+
+
+int solution(vector<int> numbers, int target) 
+{
+	int answer{ 0 };
+	int sum_value{ 0 };
+
+	DFS(numbers, target, 0, sum_value, true, answer);
+	DFS(numbers, target, 0, sum_value, false, answer);
+
+	cout << answer << endl;
+	return answer;
+}
+
+int main()
+{
+	solution({ 1, 1, 1, 1, 1 }, 3); // result: 5
+	solution({ 4, 1, 2, 1 }, 4); // result: 2
+
+	return 0;
+}
+#elif PROB == 15
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void DFS(const int& n, vector<vector<int>>& computers, int& answer, int index)
+{
+	
+	// 접근 방법을 어떻게??
+	for (int i = index; i < n; ++i)
+	{
+		if (computers[index][i] == 0)
+			answer++;			
+	}
+
+	if (index < n)
+	{
+		index += 1;
+		DFS(n, computers, answer, index++);
+	}		
+	else
+		return;
+}
+
+int solution(int n, vector<vector<int>> computers) 
+{
+	int answer{ 0 };
+
+	DFS(n, computers, answer, 0);
+	cout << answer << endl;
+
+	return answer;
+}
+
+int main()
+{
+	solution(3, { {1, 1, 0}, {1, 1, 0}, {0, 0, 1} }); // result: 2
+	solution(3, { {1, 1, 0}, {1, 1, 1}, {0, 1, 1} }); // result: 1
 
 	return 0;
 }
