b185fd3f44c4f65fad92204370497f5da0915d31
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2024-04-07 23:51:54 +0900

    Update

--- CondingTest_Solved/main.cpp
+++ CondingTest_Solved/main.cpp
@@ -1,4 +1,4 @@
-#define PROB 2
+#define PROB 3
 
 #if PROB == 1
 // [1000] A+B
@@ -25,4 +25,24 @@ int main()
 
 	return 0;
 }
+
+#elif PROB == 3
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+
+	time_t timer{ time(NULL) };
+	struct tm date{};
+	localtime_s(&date, &timer);
+
+	cout << date.tm_year + 1900 << "-" << setw(2) << setfill('0') << date.tm_mon + 1 << "-" << setw(2) << setfill('0') << date.tm_mday;
+
+	return 0;
+}
+
 #endif
