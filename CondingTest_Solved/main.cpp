8f5742a18ba46fc84da60880d7dc8485b7173b87
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2024-07-14 19:23:00 +0900

    Update

--- CondingTest_Solved/main.cpp
+++ CondingTest_Solved/main.cpp
@@ -1,48 +1,79 @@
-#define PROB 3
+#define PROB 2
 
 #if PROB == 1
-// [1000] A+B
 #include <iostream>
+#include <stack>
+#include <queue>
 using namespace std;
 
 int main()
 {
-	int input[2];
-	cin >> input[0] >> input[1];
+	stack<int> stack{};	
 
-	cout << input[0] + input[1];
+	int inputSize{};
+	cin >> inputSize;
+
+	for (int i = 0; i < inputSize; ++i)
+	{
+		string input;
+		cin >> input;
+
+		if (input == "push")
+		{
+			int value{};
+			cin >> value;
+			
+			stack.push(value);
+		}
+		else if (input == "pop")
+		{
+			bool isEmpty = stack.empty();
+
+			if (isEmpty)
+			{
+				cout << -1 << endl;
+			}
+			else
+			{				
+				cout << stack.top() << endl;
+				stack.pop();
+			}
+		}		
+		else if (input == "size")
+		{
+			cout << stack.size() << endl;
+		}
+		else if (input == "empty")
+		{
+			bool isEmpty = stack.empty();
+
+			if (isEmpty)
+			{
+				cout << 1 << endl;
+			}
+			else
+			{
+				cout << 0 << endl;
+			}
+		}
+		else if (input == "top")
+		{
+			bool isEmpty = stack.empty();
+
+			if (isEmpty)
+			{
+				cout << -1 << endl;
+			}
+			else
+			{
+				cout << stack.top() << endl;
+			}			
+		}
+	}
+
+	return 0;
 }
 #elif PROB == 2
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int input[2];
-	cin >> input[0] >> input[1];
-
-	cout << input[0] + input[1];
-
-	return 0;
-}
-
-#elif PROB == 3
-#include <iostream>
-#include <ctime>
-#include <iomanip>
-
-using namespace std;
-
-int main()
-{
-
-	time_t timer{ time(NULL) };
-	struct tm date{};
-	localtime_s(&date, &timer);
-
-	cout << date.tm_year + 1900 << "-" << setw(2) << setfill('0') << date.tm_mon + 1 << "-" << setw(2) << setfill('0') << date.tm_mday;
-
-	return 0;
-}
+// [10773] Á¦·Î
 
 #endif
