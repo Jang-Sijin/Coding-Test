9554dd97027a2b51aca98be6c8761c9651fc3ed1
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2024-07-15 08:03:37 +0900

    Update

--- CondingTest_Solved/main.cpp
+++ CondingTest_Solved/main.cpp
@@ -1,6 +1,7 @@
-#define PROB 2
+#define PROB 3
 
 #if PROB == 1
+// [10828] 스택
 #include <iostream>
 #include <stack>
 #include <queue>
@@ -75,5 +76,56 @@ int main()
 }
 #elif PROB == 2
 // [10773] 제로
+#include <iostream>
+#include <stack>
 
+using namespace std;
+
+int main()
+{
+	stack<int> result_stack{};
+
+	int input{};
+	cin >> input;
+
+	while (input--)
+	{
+		int value{};
+		cin >> value;
+
+		if (value == 0)
+		{
+			result_stack.pop();
+		}
+		else
+		{			
+			result_stack.push(value);
+		}
+	}
+
+	int result{};
+	while (!result_stack.empty())
+	{		
+		result += result_stack.top();
+		result_stack.pop();
+	}	
+
+	cout << result << endl;
+
+	return 0;
+}
+#elif PROB == 3
+// [1874] 스택 수열
+#include <iostream>
+#include <stack>
+using namespace std;
+
+// 1부터 N까지의 수를 스택에 넣었다가 뽑아 늘어놓음으로써 하나의 수열을 만들 수 있다.
+
+int main()
+{
+
+
+	return 0;
+}
 #endif
