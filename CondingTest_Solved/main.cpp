e0db5f0af08baa5a57bdabfb65a00a35f591a449
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2024-07-28 23:23:02 +0900

    Update

--- CondingTest_Solved/main.cpp
+++ CondingTest_Solved/main.cpp
@@ -1,4 +1,4 @@
-#define PROB 3
+#define PROB 6
 
 #if PROB == 1
 // [10828] 스택
@@ -9,7 +9,7 @@ using namespace std;
 
 int main()
 {
-	stack<int> stack{};	
+	stack<int> stack{};
 
 	int inputSize{};
 	cin >> inputSize;
@@ -23,7 +23,7 @@ int main()
 		{
 			int value{};
 			cin >> value;
-			
+
 			stack.push(value);
 		}
 		else if (input == "pop")
@@ -35,11 +35,11 @@ int main()
 				cout << -1 << endl;
 			}
 			else
-			{				
+			{
 				cout << stack.top() << endl;
 				stack.pop();
 			}
-		}		
+		}
 		else if (input == "size")
 		{
 			cout << stack.size() << endl;
@@ -68,7 +68,7 @@ int main()
 			else
 			{
 				cout << stack.top() << endl;
-			}			
+			}
 		}
 	}
 
@@ -98,17 +98,17 @@ int main()
 			result_stack.pop();
 		}
 		else
-		{			
+		{
 			result_stack.push(value);
 		}
 	}
 
 	int result{};
 	while (!result_stack.empty())
-	{		
+	{
 		result += result_stack.top();
 		result_stack.pop();
-	}	
+	}
 
 	cout << result << endl;
 
@@ -121,11 +121,139 @@ int main()
 using namespace std;
 
 // 1부터 N까지의 수를 스택에 넣었다가 뽑아 늘어놓음으로써 하나의 수열을 만들 수 있다.
+int main()
+{
+	// 입력 개수
+	int input{};
+	cin >> input;
+
+	string result_operator{};
+	stack<int> result_stack{};
+	int pos_value{ 1 };
+	while (input--)
+	{
+		int input_number;
+		cin >> input_number;
+
+		while (pos_value <= input_number)
+		{
+			result_stack.push(pos_value++);
+			result_operator += "+\n";
+		}
+
+		// 출력 결과 비교
+		if (result_stack.top() != input_number)
+		{
+			cout << "NO" << endl;
+			return 0;
+		}
+
+		result_stack.pop();
+		result_operator += "-\n";
+	}
+
+	cout << result_operator;
+}
+#elif PROB == 4
+// [10845] 큐
+#include <iostream>
+#include <queue>
+using namespace std;
 
 int main()
 {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
+	int input{};
+	cin >> input;
+
+	queue<int> queue{};
+	for (int i = 0; i < input; ++i)
+	{
+		string input_command{};
+		cin >> input_command;
+		if (input_command == "push")
+		{
+			int input_value{};
+			cin >> input_value;
+			queue.push(input_value);
+		}
+		else if (input_command == "pop")
+		{
+			if (queue.empty())
+				cout << -1 << "\n";
+			else
+			{
+				cout << queue.front() << "\n";
+				queue.pop();
+			}
+		}
+		else if (input_command == "size")
+		{
+			cout << queue.size() << "\n";
+		}
+		else if (input_command == "empty")
+		{
+			cout << queue.empty() << "\n";
+		}
+		else if (input_command == "front")
+		{
+			if (queue.empty())
+				cout << -1 << "\n";
+			else
+			{
+				cout << queue.front() << "\n";
+			}
+		}
+		else if (input_command == "back")
+		{
+			if (queue.empty())
+				cout << -1 << "\n";
+			else
+			{
+				cout << queue.back() << "\n";
+			}
+		}
+
+	}
 
 	return 0;
 }
+#elif PROB == 5
+// [2164] 카드2
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int input{};
+	cin >> input;
+
+	queue<int> queue;
+
+	// 1. 입력 조건 세팅
+	for (int i = 1; i <= input; ++i)
+		queue.push(i);
+
+	// 2. 로직 수행
+	while (queue.size() > 1)
+	{
+		queue.pop();
+
+		int save_value = queue.front();
+		queue.push(save_value);
+
+		queue.pop();
+	}
+
+	// 3. 결과 값 출력
+	cout << queue.front();
+}
+#elif PROB == 6
+
 #endif
